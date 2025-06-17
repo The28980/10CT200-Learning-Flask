b882621ca9887f78df30f526538c81659b8fc54e
The28980 <160200971+The28980@users.noreply.github.com>
2025-06-17 12:26:22 +1000

    Work
    Commiting some work before I run out of time

--- app/routes.py
+++ app/routes.py
@@ -1,14 +1,12 @@
 from app import app
+from flask import render_template
+
+# Use command [python -m flask run] to run this code
+# When changes to the code are made, save and refresh the instance before running
+
 @app.route('/')
 @app.route('/index')
+
 def index():
-    user = {'username': 'Miguel'}
-    return '''
-<html>
-    <head>
-        <title>Home Page - Microblog</title>
-    </head>
-    <body>
-        <h1>Hello, ''' + user['username'] + '''!</h1>
-    </body>
-</html>'''
+    user = {'username': 'Siggums'}
+    return render_template('index.html', title='Home', user=user)
